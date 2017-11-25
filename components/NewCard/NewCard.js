9b4f86544405338799d1b8f3d80454bc6a4a25c9
n0rd <nord.ptz@gmail.com>
2017-11-25 03:28:01 +0300

    Create new card feature

--- components/NewCard/NewCard.js
+++ components/NewCard/NewCard.js
@@ -1,17 +1,72 @@
 import React, { Component } from 'react';
 import { View, Text, TextInput, StyleSheet, TouchableNativeFeedback } from 'react-native';
 import { white, red } from '../../utils/colors';
+import { addCardToDeck } from "../../utils/api";
 
 class NewCard extends Component {
+  state = {
+    question: '',
+    answer: ''
+  };
+
+  saveQuestion () {
+    const { deck } = this.props.navigation.state.params;
+
+    addCardToDeck(
+      deck.title,
+      {
+        question: this.state.question,
+        answer: this.state.answer
+      }
+    );
+
+    // update redux
+  }
+
+  onQuestionChange(question) {
+    this.setState(prevState => {
+      return {
+        ...prevState,
+        question
+      }
+    })
+  }
+
+  onAnswerChange(answer) {
+    this.setState(prevState => {
+      return {
+        ...prevState,
+        answer
+      }
+    })
+  }
+
   render() {
+    const { deck } = this.props.navigation.state.params;
+
     return (
       <View style={styles.container}>
+        <Text>{deck.title}</Text>
         <View style={styles.form}>
-          <TextInput style={styles.question} placeholder="Question" editable = {true} />
-          <TextInput style={styles.answer} placeholder="Answer" editable = {true} />
+          <TextInput
+            style={styles.question}
+            placeholder="Question"
+            editable = {true}
+            onChangeText={(question) => this.onQuestionChange(question)}
+            value={this.state.question}
+          />
+          <TextInput
+            style={styles.answer}
+            placeholder="Answer"
+            editable = {true}
+            onChangeText={(answer) => this.onAnswerChange(answer)}
+            value={this.state.answer}
+          />
         </View>
 
-        <TouchableNativeFeedback background={TouchableNativeFeedback.Ripple(white)}>
+        <TouchableNativeFeedback
+          background={TouchableNativeFeedback.Ripple(white)}
+          onPress={() => this.saveQuestion()}>
           <View style={styles.addBtn}>
             <Text style={styles.addBtnText}>Add</Text>
           </View>
